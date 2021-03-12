04f790388fbc7f8c4c36a426ca29cbb8d6acd7af
bintang4974 <bintang.bintangramadhan4974@gmail.com>
2021-03-12 20:57:11 +0700

    success get all data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok('Rest API Running..');
+    response.ok('Rest API Running..', res);
+}
+
+// GET All Data Mahasiswa
+exports.getMahasiswa = function(req, res){
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, field){
+        if(error){
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
 }
